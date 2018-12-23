9edb220e7476e63ee76896272d81cc26a2285023
ke511081177 <ke511081177@gmail.com>
2018-12-24 01:44:00 +0800

    Update use picture.py

--- use picture.py
+++ use picture.py
@@ -1,33 +1,99 @@
-import sys, os, random
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
 
+This is a temporary script file.
+"""
+
+import sys, os, random
+import pyautogui as pag
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QLabel,QMainWindow,QApplication,QAction,QWidget,QHBoxLayout,QVBoxLayout,QFileDialog,QPushButton
+from PyQt5.QtCore import QSize, Qt, QMimeData, QRect, QPoint, QPointF, QLineF, QLine
+from PyQt5.QtGui import QPixmap, QDrag
 import matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
 
-class AppForm(QMainWindow):
+
+class AppForm(QMainWindow,QLabel):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
-        self.setWindowTitle('Demo: PyQt with matplotlib')
-
+        self.setGeometry(100, 100, 500, 400)
+        self.setWindowTitle('Put picture')
         self.create_menu()
         self.create_main_frame()
 
-
     def save_plot(self):
         file_choices = "PNG (*.png)|*.png"
+       
         
-        path = QFileDialog.getSaveFileName(self, 
-                        'Save file', '', 
-                        file_choices)
-        if path:
-            self.canvas.print_figure(path[0], dpi=self.dpi)
-            self.statusBar().showMessage('Saved to %s' % path, 2000)
+        openfile_path = QFileDialog.getOpenFileName(self,'選擇文件','',"PNG (*.png)|*.png")
+# =============================================================================
+#         pixmap = QPixmap(openfile_path)
+#         self.label.setPixmap(pixmap)
+#         self.resize(pixmap.height(),pixmap.width())
+# =============================================================================
+# =============================================================================
+#         self.label = QLabel(self)
+#         self.label.setPixmap(pixmap)
+#         self.label.resize(pixmap.width(), pixmap.height())
+#         self.show()
+#         #self.label.setPixmap(QPixmap(openfile_name))
+# =============================================================================
+        #self.label.setGeometry(60,50,800,400)
+        
+
+
+        print(pixmap)
+        
+
+# =============================================================================
+#     
+#     def mousePressEvent(self,e):
+#         if e.buttons() != Qt.RightButton:
+#             return
+#         mimeData = QMimeData()
+#         drag = QDrag(self)
+#         drag.setMimeData(mimeData)
+#         drag.setHotSpot(e.pos() - self.rect().topLeft())
+#         drag.exec_(Qt.MoveAction)
+# 
+#     def mousePressEvent(self, e):
+#         #self.label.mousePressEvent(self,e)
+#         if e.button() == Qt.LeftButton:
+#             print('press')
+# 
+#     def dragEnterEvent(self, e):
+#         e.accept()
+# 
+#     def dropEvent(self, e):
+#         position = e.pos()
+#         self.label.move(position)
+# 
+#         e.setDropAction(Qt.MoveAction)
+#         e.accept()
+# 
+# =============================================================================
+        
+    def mouseMoveEvent(self, event):
+        print("On Hover") # event.pos().x(), event.pos().y()
+        if event.buttons() != Qt.RightButton:
+            self.label.pos = pag.position
+            print(self.label.pos)
+            #self.label.pos = event.pos
+          
+    def mousePressEvent(self, event):
+        print(event)
+        
+    
+
+  
+    
     
     def on_about(self):
         msg ="""
@@ -36,102 +102,49 @@ class AppForm(QMainWindow):
         """
         QMessageBox.about(self, "簡介", msg.strip())
     
-    def on_pick(self, event):
-        # The event received here is of the type
-        # matplotlib.backend_bases.PickEvent
-        #
-        # It carries lots of information, of which we're using
-        # only a small amount here.
-        # 
-        box_points = event.artist.get_bbox().get_points()
-        msg = "You've clicked on a bar with coords:\n %s" % box_points
-        
-        QMessageBox.information(self, "Click!", msg)
-    
-    def on_draw(self):
-        """ Redraws the figure
-        """
-        #str = unicode(self.textbox.text())
-        self.data = list(map(int, self.textbox.text().split()))
-        
-        x = range(len(self.data))
 
-        # clear the axes and redraw the plot anew
-        #
-        self.axes.clear()        
-        self.axes.grid(self.grid_cb.isChecked())
-        
-        self.axes.bar(
-            left=x, 
-            height=self.data, 
-            width=self.slider.value() / 100.0, 
-            align='center', 
-            alpha=0.44,
-            picker=5)
-        
-        self.canvas.draw()
-    
     def create_main_frame(self):
         self.main_frame = QWidget()
+        self.main_frame.resize(300,400)
         
-        # Create the mpl Figure and FigCanvas objects. 
-        # 5x4 inches, 100 dots-per-inch
-        #
-        self.dpi = 100
-        self.fig = Figure((5.0, 4.0), dpi=self.dpi)
-        self.canvas = FigureCanvas(self.fig)
-        self.canvas.setParent(self.main_frame)
+        self.label = QLabel(self)
+        self.label.setGeometry(40,40,800,600)
+        self.label.setText('左')
+
+         
+        pixmap = QPixmap('image.png')
+        self.label.setPixmap(pixmap)
+        self.resize(1200,800)
+
+
+
+
+# =============================================================================
+#         self.canvas = FigureCanvas(self.fig)
+#         self.canvas.setParent(self.main_frame)
+# =============================================================================
+       
         
-        # Since we have only one plot, we can use add_axes 
-        # instead of add_subplot, but then the subplot
-        # configuration tool in the navigation toolbar wouldn't
-        # work.
-        #
-        self.axes = self.fig.add_subplot(111)
-        
-        # Bind the 'pick' event for clicking on one of the bars
-        #
-        self.canvas.mpl_connect('pick_event', self.on_pick)
-        
-        # Create the navigation toolbar, tied to the canvas
-        #
-        self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
-        
-        # Other GUI controls
-        # 
-        self.textbox = QLineEdit()
-        self.textbox.setMinimumWidth(200)
-        self.textbox.editingFinished.connect(self.on_draw)
-        
-        self.draw_button = QPushButton("&Draw")
-        self.draw_button.clicked.connect(self.on_draw)
+
+
+
         
         
-        self.grid_cb = QCheckBox("Show &Grid")
-        self.grid_cb.setChecked(False)
-        self.grid_cb.stateChanged.connect(self.on_draw) #int
+ # =================================================================
+#         self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
+# =============================================================================
         
-        slider_label = QLabel('Bar width (%):')
-        self.slider = QSlider(Qt.Horizontal)
-        self.slider.setRange(1, 100)
-        self.slider.setValue(20)
-        self.slider.setTracking(True)
-        self.slider.setTickPosition(QSlider.TicksBothSides)
-        self.slider.valueChanged.connect(self.on_draw)#int
-        
-        #
-        # Layout with box sizers
-        # 
+
+
         hbox = QHBoxLayout()
         
-        for w in [  self.textbox, self.draw_button, self.grid_cb,
-                    slider_label, self.slider]:
-            hbox.addWidget(w)
-            hbox.setAlignment(w, Qt.AlignVCenter)
+
         
         vbox = QVBoxLayout()
-        vbox.addWidget(self.mpl_toolbar)
-        vbox.addWidget(self.canvas)
+# =============================================================================
+#         vbox.addWidget(self.mpl_toolbar)
+#         vbox.addWidget(self.canvas)
+# =============================================================================
         vbox.addLayout(hbox)
         
         self.main_frame.setLayout(vbox)
@@ -141,9 +154,213 @@ class AppForm(QMainWindow):
     def create_menu(self):        
         self.file_menu = self.menuBar().addMenu("&File")
         
-        load_file_action = self.create_action("&Save plot",
+        load_file_action = self.create_action("&Open plot",
             shortcut="Ctrl+S", slot=self.save_plot, 
-            tip="Save the plot")
+            tip="Open the plot")
+        quit_action = self.create_action("&Quit", slot=self.close, 
+            shortcut="Ctrl+Q", tip="Close the application")
+        
+        self.add_actions(self.file_menu, 
+            (load_file_action, None, quit_action))
+        
+        self.help_menu = self.menuBar().addMenu("&Help")
+        about_action = self.create_action("&About", 
+            shortcut='F1', slot=self.on_about, 
+            tip='About the demo')
+        
+        self.add_actions(self.help_menu, (about_action,))
+
+    def add_actions(self, target, actions):
+        for action in actions:
+            if action is None:
+                target.addSeparator()
+            else:
+                target.addAction(action)
+
+    def create_action(  self, text, slot=None, shortcut=None, 
+                        icon=None, tip=None, checkable=False, 
+                        signal="triggered()"):
+        action = QAction(text, self)
+        if icon is not None:
+            action.setIcon(QIcon(":/%s.png" % icon))
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        if tip is not None:
+            action.setToolTip(tip)
+            action.setStatusTip(tip)
+        if slot is not None:
+            action.triggered.connect(slot)
+        if checkable:
+            action.setCheckable(True)
+        return action
+
+
+def main():
+    app = QApplication(sys.argv)
+    form = AppForm()
+    form.show()
+    sys.exit(app.exec_())
+    
+if __name__ == "__main__":
+    main()# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import sys, os, random
+import pyautogui as pag
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QLabel,QMainWindow,QApplication,QAction,QWidget,QHBoxLayout,QVBoxLayout,QFileDialog,QPushButton
+from PyQt5.QtCore import QSize, Qt, QMimeData, QRect, QPoint, QPointF, QLineF, QLine
+from PyQt5.QtGui import QPixmap, QDrag
+import matplotlib
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+
+
+class AppForm(QMainWindow,QLabel):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.setGeometry(100, 100, 500, 400)
+        self.setWindowTitle('Put picture')
+        self.create_menu()
+        self.create_main_frame()
+
+    def save_plot(self):
+        file_choices = "PNG (*.png)|*.png"
+       
+        
+        openfile_path = QFileDialog.getOpenFileName(self,'選擇文件','',"PNG (*.png)|*.png")
+# =============================================================================
+#         pixmap = QPixmap(openfile_path)
+#         self.label.setPixmap(pixmap)
+#         self.resize(pixmap.height(),pixmap.width())
+# =============================================================================
+# =============================================================================
+#         self.label = QLabel(self)
+#         self.label.setPixmap(pixmap)
+#         self.label.resize(pixmap.width(), pixmap.height())
+#         self.show()
+#         #self.label.setPixmap(QPixmap(openfile_name))
+# =============================================================================
+        #self.label.setGeometry(60,50,800,400)
+        
+
+
+        print(pixmap)
+        
+
+# =============================================================================
+#     
+#     def mousePressEvent(self,e):
+#         if e.buttons() != Qt.RightButton:
+#             return
+#         mimeData = QMimeData()
+#         drag = QDrag(self)
+#         drag.setMimeData(mimeData)
+#         drag.setHotSpot(e.pos() - self.rect().topLeft())
+#         drag.exec_(Qt.MoveAction)
+# 
+#     def mousePressEvent(self, e):
+#         #self.label.mousePressEvent(self,e)
+#         if e.button() == Qt.LeftButton:
+#             print('press')
+# 
+#     def dragEnterEvent(self, e):
+#         e.accept()
+# 
+#     def dropEvent(self, e):
+#         position = e.pos()
+#         self.label.move(position)
+# 
+#         e.setDropAction(Qt.MoveAction)
+#         e.accept()
+# 
+# =============================================================================
+        
+    def mouseMoveEvent(self, event):
+        print("On Hover") # event.pos().x(), event.pos().y()
+        if event.buttons() != Qt.RightButton:
+            self.label.pos = pag.position
+            print(self.label.pos)
+            #self.label.pos = event.pos
+          
+    def mousePressEvent(self, event):
+        print(event)
+        
+    
+
+  
+    
+    
+    def on_about(self):
+        msg ="""
+        
+        *可以把開個照片，然後丟進來拉來拉去這樣。
+        """
+        QMessageBox.about(self, "簡介", msg.strip())
+    
+
+    def create_main_frame(self):
+        self.main_frame = QWidget()
+        self.main_frame.resize(300,400)
+        
+        self.label = QLabel(self)
+        self.label.setGeometry(40,40,800,600)
+        self.label.setText('左')
+
+         
+        pixmap = QPixmap('image.png')
+        self.label.setPixmap(pixmap)
+        self.resize(1200,800)
+
+
+
+
+# =============================================================================
+#         self.canvas = FigureCanvas(self.fig)
+#         self.canvas.setParent(self.main_frame)
+# =============================================================================
+       
+        
+
+
+
+        
+        
+ # =================================================================
+#         self.mpl_toolbar = NavigationToolbar(self.canvas, self.main_frame)
+# =============================================================================
+        
+
+
+        hbox = QHBoxLayout()
+        
+
+        
+        vbox = QVBoxLayout()
+# =============================================================================
+#         vbox.addWidget(self.mpl_toolbar)
+#         vbox.addWidget(self.canvas)
+# =============================================================================
+        vbox.addLayout(hbox)
+        
+        self.main_frame.setLayout(vbox)
+        self.setCentralWidget(self.main_frame)
+    
+        
+    def create_menu(self):        
+        self.file_menu = self.menuBar().addMenu("&File")
+        
+        load_file_action = self.create_action("&Open plot",
+            shortcut="Ctrl+S", slot=self.save_plot, 
+            tip="Open the plot")
         quit_action = self.create_action("&Quit", slot=self.close, 
             shortcut="Ctrl+Q", tip="Close the application")
         
